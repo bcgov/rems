8da677b355f5e949e4d1a36a8f3bcd844678c2cb
Andy Teucher <andy.teucher@gov.bc.ca>
2016-08-31 12:15:22 -0700

    Move more functions to utils.R

--- R/get_ems_data.R
+++ R/get_ems_data.R
@@ -120,11 +120,7 @@ download_ems_data <- function(url) {
   unzip(res$request$output$path, exdir = csvdir)
 }
 
-httr_progress <- function() {
-  if (interactive()) {
-    return(httr::progress("down"))
-  }
-}
+
 
 #' @importFrom httr GET content
 get_file_metadata <- function() {
@@ -141,45 +137,6 @@ get_file_metadata <- function() {
   rownames(files_df) <- ifelse(grepl("\\d.+_current", files_df$filename), "3yr_current", ifelse(grepl("current", files_df$filename), "current", "historic"))
   files_df$date_upd <- strptime(files_df$date_upd, format = "%m/%d/%Y %R %p")
   files_df
-}
-
-base_url <- function() {
-  "https://pub.data.gov.bc.ca/datasets/949f2233-9612-4b06-92a9-903e817da659"
-}
-
-#' Remove cached EMS data from your computer
-#'
-#' @param which which data to remove? Either \code{"current"}, \code{"historic"},
-#' or \code{"all"}.
-#'
-#' @return NULL
-#' @export
-remove_data_cache <- function(which) {
-  if (!which %in% c("all", "current", "historic")) {
-    stop("'which' must be one of 'all', 'current', 'historic'")
-  }
-  message("Removing ", which, " data from your local cache...")
-  if (which == "all") {
-    remove_it("historic")
-    burn_it_down()
-  } else {
-    remove_it(which)
-  }
-
-  invisible(NULL)
-}
-
-remove_it <- function(which) {
-  if (which == "historic") {
-    fpath <- write_db_path()
-    if (file.exists(fpath)) {
-      file.remove(write_db_path())
-    }
-  } else if (which == "current") {
-    cache <- ._remsCache_
-    cache$del(which)
-  }
-  set_update_date(which, value = NULL)
 }
 
 set_update_date <- function(which, value) {
