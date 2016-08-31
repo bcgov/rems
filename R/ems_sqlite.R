52abab044818bb3acc4c7af2b662297d8faaa988
Andy Teucher <andy.teucher@gov.bc.ca>
2016-08-31 15:41:42 -0700

    Add license header

--- R/ems_sqlite.R
+++ R/ems_sqlite.R
@@ -1,3 +1,15 @@
+# Copyright 2016 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
 #' Download and store the large historic ems dataset
 #'
 #' @param n the chunk size to use to iteratively read and store the historic data (default 1 million)
