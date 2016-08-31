1c3f63bad1fdd2e1a2124263b7c12d24587ba4e1
Andy Teucher <andy.teucher@gov.bc.ca>
2016-08-31 12:09:34 -0700

    Move some functions to utils.R file

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,27 +1,5 @@
 ._remsCache_ <- NULL
 
-write_cache <- function() {
-  path <- rappdirs::user_data_dir("rems")
-  cache <- storr::storr_rds(path, compress = FALSE, default_namespace = "rems")
-  cache
-}
-
 .onLoad <- function(libname, pkgname) {
   ._remsCache_ <<- write_cache()
-}
-
-#' Destroy data cache
-#'
-#' Use this if you are getting odd errors when trying to get data or update dates
-#' from the cache
-#'
-#' @return TRUE
-#' @noRd
-burn_it_down <- function() {
-  if (file.exists(rappdirs::user_data_dir("rems"))) {
-    ._remsCache_$destroy()
-  }
-  unlockBinding("._remsCache_", getNamespace("rems"))
-  ._remsCache_ <<- write_cache()
-  lockBinding("._remsCache_", getNamespace("rems"))
 }
