cab134ceb0c345c1a18d1b366408f455676093e3
Andy Teucher <andy.teucher@gmail.com>
2019-10-11 16:33:24 -0700

    Comments and tweaks

--- inst/create_historic/create_historic_sqlite.R
+++ inst/create_historic/create_historic_sqlite.R
@@ -1,13 +1,11 @@
 
+# Download csv, create sqlite, and zip it.
 url <- "https://pub.data.gov.bc.ca/datasets/949f2233-9612-4b06-92a9-903e817da659/ems_sample_results_historic_expanded.csv"
-
 csv_file <- rems:::download_ems_data(url)
-
-db_file <- "/Users/ateucher/ems_historic_2019-10-11.sqlite"
-
+db_file <- path.expand("~/ems_historic.sqlite")
 rems:::save_historic_data(csv_file, db_file, 1e6)
-
 zip(paste0(db_file, ".zip"), db_file)
+# Then manually attach to release
 
 
 # Test getting it
