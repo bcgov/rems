6edc0317e5bd7a5b14cc16727804f16b8171bc6f
Andy Teucher <andy.teucher@gmail.com>
2019-10-16 10:39:07 -0700

    Update script to create historic sqlite db

--- inst/create_historic/create_historic_sqlite.R
+++ inst/create_historic/create_historic_sqlite.R
@@ -2,11 +2,14 @@
 # Download csv, create sqlite, and zip it.
 url <- "https://pub.data.gov.bc.ca/datasets/949f2233-9612-4b06-92a9-903e817da659/ems_sample_results_historic_expanded.csv"
 csv_file <- rems:::download_ems_data(url)
-db_file <- path.expand("~/ems_historic.sqlite")
+db_file <- path.expand("~/Desktop/ems_historic.sqlite")
 rems:::save_historic_data(csv_file, db_file, 1e6)
-zip(paste0(db_file, ".zip"), db_file)
-# Then manually attach to release (can change to doing it via api a la bcmapsdata)
+zipfile <- paste0(db_file, ".zip")
+zip::zipr(zipfile, db_file, include_directories = FALSE)
 
+# Then manually attach to release (can change to doing it via api a la bcmapsdata)
+# This isn't working as errors with 413 (file too large)
+rems:::upload_release_asset(zipfile)
 
 # Test getting it
 rems:::download_file_from_release("ems_historic.sqlite.zip",
