617e2b60d36a6a9b7194946aa969392054bb0ad1
Andy Teucher <andy.teucher@gov.bc.ca>
2021-01-07 16:09:13 -0800

    Fix ems timezone tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,26 +1,10 @@
 context("utils")
 
-test_that("ems_posix_numeric works with integer, integer64, numeric", {
-  dt <- as.POSIXct("2018-02-05 14:35", tz = "Etc/GMT+8")
-  dt_int <- as.integer(dt)
-  expect_equal(ems_posix_numeric(dt_int), dt)
-  dt_int64 <- bit64::as.integer64(dt_int)
-  expect_equal(ems_posix_numeric(dt_int64), dt)
-  dt_numeric <- as.double(dt)
-  expect_equal(ems_posix_numeric(dt_numeric), dt)
-})
-
-test_that("ems_posixct works with all classes", {
+test_that("set_ems_tz works with all classes", {
   datestring <- "2019-11-18"
   expected <- as.POSIXct(datestring, tz = "Etc/GMT+8")
-  expect_equal(ems_posixct(datestring), expected)
-  expect_equal(ems_posixct(as.Date(datestring)), expected)
-  expect_equal(ems_posixct(expected), expected)
-  expect_equal(ems_posixct(as.POSIXct("2019-11-18 08:00:00", tz = "UTC")),
-    expected)
-  expect_equal(ems_posixct(as.POSIXlt(datestring, tz = "Etc/GMT+8")), expected)
-  expect_equal(ems_posixct(as.POSIXlt("2019-11-18 08:00:00", tz = "UTC")),
-    expected)
-  expect_equal(ems_posixct(1574064000), expected)
-  expect_error(ems_posixct(list()), "No ems_posixct method defined for objects of class 'list'")
+  expect_equal(set_ems_tz(datestring), expected)
+  expect_equal(set_ems_tz(as.Date(datestring)), expected)
+  expect_equal(set_ems_tz(expected), expected)
+  expect_equal(set_ems_tz(as.POSIXlt(datestring, tz = "Etc/GMT+8")), expected)
 })
