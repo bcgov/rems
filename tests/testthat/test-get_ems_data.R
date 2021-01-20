e3a8b0d55608f9868a7a903db9f49f471526ef6b
Andy Teucher <andy.teucher@gov.bc.ca>
2021-01-20 11:50:25 -0800

    update tests

--- tests/testthat/test-get_ems_data.R
+++ tests/testthat/test-get_ems_data.R
@@ -4,19 +4,22 @@ test_that("reading metadata works", {
   skip_on_cran()
   ret <- get_databc_metadata()
   expect_is(ret, "data.frame")
-  expect_equal(dim(ret), c(3, 3))
+  expect_equal(dim(ret), c(5, 4))
   expect_equal(lapply(ret, class),
     list(filename = "character",
       server_date = c("POSIXct", "POSIXt"),
-      label = "character"))
-  expect_false(any(duplicated(ret$label)))
+      label = "character",
+      filetype = "character"))
+  expect_false(any(duplicated(ret$filename)))
 
   # Test extracting for each
-  expect_equal(nrow(get_file_metadata("historic")), 1L)
+  expect_equal(get_file_metadata("historic")[["filetype"]], "csv")
+  expect_equal(get_file_metadata("historic", "zip")[["filetype"]], "zip")
 
-  expect_equal(nrow(get_file_metadata("2yr")), 1L)
+  expect_equal(get_file_metadata("2yr")[["filetype"]], "csv")
 
-  expect_equal(nrow(get_file_metadata("4yr")), 1L)
+  expect_equal(get_file_metadata("4yr")[["filetype"]], "csv")
+  expect_equal(get_file_metadata("4yr", "zip")[["filetype"]], "zip")
 
   expect_error(get_file_metadata("foo"), "'which' needs to be one of")
 })
