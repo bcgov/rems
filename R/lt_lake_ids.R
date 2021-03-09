dd36d528e0acbedaa710a121ff17aa7c8d4547ee
Karly Harker <karly.harker@gov.bc.ca>
2021-03-09 13:36:43 -0800

    documentation, files for long-term lake dataset

---
+++ R/lt_lake_ids.R
@@ -0,0 +1,12 @@
+#' Active lake monitoring sites as of 09-03-2021.
+#'
+#' A dataset containing the EMS ID, Site Name, and Requisition IDs of all active lake monitoring stations.
+#'
+#' @format A data frame with 1307 rows and 3 variables
+#' \describe{
+#'   \item{EMS_ID}{EMS IDs of sites}
+#'   \item{MONITORING_LOCATION}{names of sites}
+#'   \item{REQUISTION_ID}{Requisition IDS of sites}
+#'   }
+#' @source internal data source
+"lt_lake_ids"
