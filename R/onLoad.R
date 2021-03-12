4d748ea7efdcb06d843c960b855b46e6349b8915
Andy Teucher <andy.teucher@gov.bc.ca>
2021-03-12 13:15:14 -0800

    Use new install_unit over deprecated functions

--- R/onLoad.R
+++ R/onLoad.R
@@ -14,32 +14,36 @@
 
 register_ems_units <- function() {
 
+  # Test if one is installed - if it is, it means the
+  # package has been loaded once already so don't try
+  # to do it again
   try_test <- try(
-    units::install_conversion_constant("S", "mho", 1),
+    units::install_unit("mho", "1 S"),
     silent = TRUE
   )
 
   if (!inherits(try_test, "try-error")) {
-    units::install_symbolic_unit("MPN")
-    units::install_conversion_constant("MPN", "CFU", 1)
-    units::install_symbolic_unit("NTU")
-    units::install_conversion_constant("NTU", "JTU", 1)
-    units::install_conversion_constant("US_liquid_gallon", "USG", 1)
-    units::install_conversion_constant("UK_liquid_gallon", "IG", 1)
+    units::install_unit("MPN", name = "Most Probable Number")
+    units::install_unit("CFU", "1 MPN", "Colony-Forming Unit")
+    units::install_unit("NTU", name = "Nephelometric Turbidity Unit")
+    units::install_unit("JTU", "1 NTU", "Jackson Turbidity Unit")
+    units::install_unit("USG", "1 US_liquid_gallon", "US Gallon")
+    units::install_unit("IG", "1 UK_liquid_gallon", "Imperial Gallon")
     # These petroleum measures are actually only ever used
     # as volumes (1m3 = 0.001 E3m3 = 1e-6), but can't use E3m3
     # to install the unit so have to do it as m, units takes
     # care of the rest when it is m3 <=> E3m3 etc.
-    units::install_conversion_constant("t", "adt", 1)
-    units::install_conversion_constant("m", "E3m", .1)
-    units::install_conversion_constant("m", "E6m", .01)
-    units::install_conversion_constant("L", "E6L", 1e-6)
-    units::install_conversion_constant("UK_liquid_gallon", "E6IG", 1e-6)
+    units::install_unit("adt", "1 t", "Air Dry Tonne")
+    units::install_unit("E3m", "10 m")
+    units::install_unit("E6m", "100 m")
+    units::install_unit("E6L", "1e6 L")
+    units::install_unit("E6IG", "1e6 UK_liquid_gallon")
     #> set_units(set_units(1, "m3"), "E3m3")
     # 0.001 [E3m3]
     #> set_units(set_units(1, "m3"), "E6m3")
     # 1e-06 [E6m3]
   }
+  invisible(NULL)
 }
 
 .onLoad <- function(libname, pkgname) {
