9d488a7500fc33f6a1edae150e132d27dc071a96
Andy Teucher <andy.teucher@gov.bc.ca>
2021-03-23 16:06:17 -0700

    remove duplicated test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -13,11 +13,6 @@ test_that("basic utils work", {
   expect_equal(attr(add_rems_type(list(), "2yr"), "rems_type"), "2yr")
   expect_error(add_rems_type(list(), "hello"))
 
-  tmp <- tempfile()
-  writeLines("hello", tmp)
-  on.exit(unlink(tmp))
-  expect_equal(nchar(make_file_hash(tmp)), 40L)
-
   skip_on_cran()
   skip_if_offline()
   expect_equal(httr::status_code(httr::GET(base_url())), 200)
